662e566d1131d9e5a2d1bdca547767fe553d9197
KDK <kdk9678@gmail.com>
2024-03-17 20:11:07 +0900

    Successful build

--- robot.cpp
+++ robot.cpp
@@ -41,14 +41,14 @@ Robot::Robot():
     double cgoal[SIZE_STATE];
 
     con.getPos(pos);
-    con.addGoal(2.5,2.0,0.0);//add global goal
+    con.addGoal(3.0,3.0,0.0);//add global goal
     con.getGoal(cgoal,true);
 
     sen=new Sensor(is);
-    // sen->addLObs(1.8,2.52,1.79,2.72);
-    // sen->addLObs(1.8,2.52,2.5,2.5);
-    // sen->addLObs(2.5,2.5,2.48,1.8);
-    // sen->addLObs(2.48,1.8,2.68,1.81);
+    sen->addLObs(1.8,2.52,1.79,2.72);
+    sen->addLObs(1.8,2.52,2.5,2.5);
+    sen->addLObs(2.5,2.5,2.48,1.8);
+    sen->addLObs(2.48,1.8,2.68,1.81);
 
     // sen->addCObs(2.0,6.0,1.0);
     // sen->addCObs(3.0,1.5,1.0);
@@ -59,13 +59,13 @@ Robot::Robot():
     // sen->addCObs(5.0,6.0,1.0);
     // sen->addCObs(6.0,6.0,1.0);
 
-    sen->addLObs(1.7,1.7,1.55,1.85);
-    sen->addLObs(1.55,1.85,1.85,2.15);
-    sen->addLObs(1.7,1.7,2.0,2.0);
-    sen->addLObs(2.0,2.0,2.4,1.6);
-    sen->addLObs(2.4,1.6,2.1,1.3);
-    sen->addLObs(2.1,1.3,2.25,1.15);
-    sen->addLObs(2.25,1.15,2.55,1.45);
+    // sen->addLObs(1.7,1.7,1.55,1.85);
+    // sen->addLObs(1.55,1.85,1.85,2.15);
+    // sen->addLObs(1.7,1.7,2.0,2.0);
+    // sen->addLObs(2.0,2.0,2.4,1.6);
+    // sen->addLObs(2.4,1.6,2.1,1.3);
+    // sen->addLObs(2.1,1.3,2.25,1.15);
+    // sen->addLObs(2.25,1.15,2.55,1.45);
 
     act=new Actuator(ia);
     gen=new Generator(ig,*sen,pos,cgoal);
