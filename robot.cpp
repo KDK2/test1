f38e08e1343b96b5c7a2f695b090aff6988c0c8c
KDK <kdk9678@gmail.com>
2024-03-17 18:58:08 +0900

    Successful build

--- robot.cpp
+++ robot.cpp
@@ -28,7 +28,7 @@ Robot::Robot():
     ig.f_param.qparam.q_q=-1.0;
 
     ig.p_param.lparam.delta=0.05;
-    ig.p_param.lparam.lam=2.0;
+    ig.p_param.lparam.lam=3.0;
     ig.p_param.lparam.lam_stagnation=0.25*ig.p_param.lparam.lam;
     ig.p_param.lparam.radius=0.25*ig.p_param.lparam.lam_stagnation;
 
